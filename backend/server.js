77279ea09d9b353db03c1aae7d1459ffae43ac7e
vitalya57 <igorpendrak4@gmail.com>
2025-06-15 17:49:28 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-
+const app = express();
 const authRoutes = require('./routes/auth');
 const commentRoutes = require('./routes/comments');
 
